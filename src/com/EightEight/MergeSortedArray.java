fe33cea400456058df78bc5164464d95c45ec075
zhm <306490609@qq.com>
2020-08-01 01:14:47 +0800

    Solusion for 88
    Merge two sorted array using existing array

---
+++ src/com/EightEight/MergeSortedArray.java
@@ -0,0 +1,35 @@
+package com.EightEight;
+
+/**
+ * Created by zhm on 2020/7/31.
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m - 1, j = n - 1, index = m + n - 1; index >= 0; index--) {
+            if (i < 0) {
+                nums1[index] = nums2[j--];
+            } else if (j < 0) {
+                nums1[index] = nums1[i--];
+            } else if (nums1[i] <= nums2[j]) {
+                nums1[index] = nums2[j--];
+            } else if (nums1[i] > nums2[j]) {
+                nums1[index] = nums1[i--];
+            }
+        }
+        //If can use another array
+        /*int[] result = new int[m + n];
+
+        for (int i = 0, j = 0, index = 0; index < m + n; index++) {
+            if (i == m) {
+                result[index] = nums2[j++];
+            } else if (j == n) {
+                result[index] = nums1[i++];
+            } else if (nums1[i] < nums2[j]) {
+                result[index] = nums1[i++];
+            } else if (nums1[i] >= nums2[j]) {
+                result[index] = nums2[j++];
+            }
+        }
+        return result;*/
+    }
+}
