207a1f1b73125a35a89592d7faf316031cf0bd5a
zhm <306490609@qq.com>
2020-08-01 19:46:34 +0800

    Optimize 88 solution

--- src/com/EightEight/MergeSortedArray.java
+++ src/com/EightEight/MergeSortedArray.java
@@ -6,14 +6,13 @@ package com.EightEight;
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for (int i = m - 1, j = n - 1, index = m + n - 1; index >= 0; index--) {
-            if (i < 0) {
-                nums1[index] = nums2[j--];
-            } else if (j < 0) {
+            if (j < 0) {
+                break;
+            }
+            if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[index] = nums1[i--];
-            } else if (nums1[i] <= nums2[j]) {
+            } else {
                 nums1[index] = nums2[j--];
-            } else if (nums1[i] > nums2[j]) {
-                nums1[index] = nums1[i--];
             }
         }
         //If can use another array
