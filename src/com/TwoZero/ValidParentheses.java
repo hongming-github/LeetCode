b2c7f8ed74eba9bfb75a17d34582247ad9e7cff3
zhm <306490609@qq.com>
2020-08-02 11:34:59 +0800

    Optimized 20
    checking string length first

--- src/com/TwoZero/ValidParentheses.java
+++ src/com/TwoZero/ValidParentheses.java
@@ -7,30 +7,32 @@ import java.util.Stack;
  */
 public class ValidParentheses {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
+        if ((s.length() & 1) == 1) return false;
+        else {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(' || c == '{' || c == '[') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
 
-                if (c == ')' && stack.pop() != '(') {
-                    return false;
-                }
+                    if (c == ')' && stack.pop() != '(') {
+                        return false;
+                    }
 
-                if (c == ']' && stack.pop() != '[') {
-                    return false;
-                }
+                    if (c == ']' && stack.pop() != '[') {
+                        return false;
+                    }
 
-                if (c == '}' && stack.pop() != '{') {
-                    return false;
+                    if (c == '}' && stack.pop() != '{') {
+                        return false;
+                    }
                 }
             }
+            return stack.isEmpty();
         }
-        return stack.isEmpty();
-
     }
 }
