3c682fe434887cea78a7c3a6358e39b741620259
zhm <306490609@qq.com>
2020-10-05 01:17:20 +0800

    101 BFS

--- src/com/OneZeroOne/SymmetricTree.java
+++ src/com/OneZeroOne/SymmetricTree.java
@@ -1,13 +1,40 @@
 package com.OneZeroOne;
 
-import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by zhm on 2020/10/3.
  */
 public class SymmetricTree {
+    //BFS Iterative
     public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root.left);
+        queue.offer(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode leftNode = queue.poll();
+            TreeNode rightNode = queue.poll();
+
+            if (leftNode == null && rightNode == null) continue;
+            if (leftNode == null || rightNode == null) return false;
+
+            if (leftNode.val != rightNode.val) return false;
+
+            queue.offer(leftNode.left);
+            queue.offer(rightNode.right);
+            queue.offer(leftNode.right);
+            queue.offer(rightNode.left);
+        }
+        return true;
+    }
+
+    //DFS Recursive
+    /*public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
         return isSymmetric(root.left, root.right);
     }
@@ -16,7 +43,7 @@ public class SymmetricTree {
         if (left == null || right == null) return left == right;
         if (left.val != right.val) return false;
         return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
-    }
+    }*/
 
 
     public class TreeNode {
