0fbcdb945ce5677704da32c9fddd3c60a712d474
zhm <306490609@qq.com>
2020-10-05 21:03:24 +0800

    BFS for 111

--- src/com/OneOneOne/MinimumDepthOfBinaryTree.java
+++ src/com/OneOneOne/MinimumDepthOfBinaryTree.java
@@ -1,9 +1,37 @@
 package com.OneOneOne;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by zhm on 2020/9/28.
  */
 public class MinimumDepthOfBinaryTree {
+    //BFS
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int minDepth = 0;
+        while (!q.isEmpty()) {
+            minDepth++;
+            int count = 0;
+            int levelSize = q.size();
+            while (count < levelSize) {
+                TreeNode node = q.poll();
+                count++;
+                TreeNode leftNode = node.left;
+                TreeNode rightNode = node.right;
+                if (leftNode == null && rightNode == null) return minDepth;
+                if (leftNode != null) q.offer(leftNode);
+                if (rightNode != null) q.offer(rightNode);
+            }
+        }
+        return minDepth;
+    }
+
+
+    /*
     //DFS
     public int minDepth(TreeNode root) {
         if (root == null)
@@ -19,7 +47,7 @@ public class MinimumDepthOfBinaryTree {
             return minRight;
         }
         return Math.min(minLeft, minRight);
-    }
+    }*/
 
     public class TreeNode {
         int val;
